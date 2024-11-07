85ef001c813a658ffecbf4b20d908d9706f38270
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-07 23:55:39 +0900

    Create: textarea module

---
+++ feature/textarea/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+    }
+}
---
+++ feature/textarea/src/commonMain/kotlin/love/aespa/nemomemo/textarea/MyClass.kt
@@ -0,0 +1,7 @@
+package love.aespa.nemomemo.textarea
+
+class MyClass {
+    fun sayHello(): String {
+        return "Hello from shared module!"
+    }
+}
