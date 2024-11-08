da8cf488b28c480804353772d358a339a70ac01a
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-09 01:44:40 +0900

    Add example textfield

---
+++ feature/textarea/src/commonMain/kotlin/love/aespa/nemomemo/textarea/ui/TextArea.kt
@@ -0,0 +1,34 @@
+package love.aespa.nemomemo.textarea.ui
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun textArea() {
+    val text = rememberSaveable { mutableStateOf("asdfsdf") }
+    TextField(
+        value = text.value,
+        onValueChange = { text.value = it }, modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(10.dp)
+            .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(8.dp))
+    )
+}
+
+@Composable
+@Preview
+fun textAreaPreview() {
+    textArea()
+}
