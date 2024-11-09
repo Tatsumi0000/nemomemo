c54336fd33bcf1b06130b036ac4bf5d6c1ea53b8
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-10 03:19:00 +0900

    Add DI initializer

--- composeApp/src/desktopMain/kotlin/love/aespa/nemomemo/App.kt
+++ composeApp/src/desktopMain/kotlin/love/aespa/nemomemo/App.kt
@@ -10,19 +10,44 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+import love.aespa.nemomemo.database.entity.Memo
+import love.aespa.nemomemo.di.appModule
+import love.aespa.nemomemo.repository.MemoRepository
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import nemomemo.composeapp.generated.resources.Res
 import nemomemo.composeapp.generated.resources.compose_multiplatform
+import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
+import java.util.Date
 
 @Composable
 @Preview
 fun App() {
+    KoinApplication(application = {
+        modules(appModule())
+    }) {
+    val repository = koinInject<MemoRepository>()
+
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        val composableScope = rememberCoroutineScope()
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
+            Button(onClick = {
+                showContent = !showContent
+                val memo = Memo(
+                    text = "aaa",
+                    createdAt = Date(),
+                    updatedAd = Date()
+                )
+                composableScope.launch {
+                    repository.insert(memo)
+                    repository.getAllMemosOrderByIdAsc()
+                }
+            }) {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
@@ -33,5 +58,6 @@ fun App() {
                 }
             }
         }
+    }
     }
 }
