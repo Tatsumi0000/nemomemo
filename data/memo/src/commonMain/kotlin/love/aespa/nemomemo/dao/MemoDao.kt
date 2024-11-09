d50cb99976b353a17ea165676308708adc7f9e70
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-09 23:27:52 +0900

    Move dao database dir to dao dir

---
+++ data/memo/src/commonMain/kotlin/love/aespa/nemomemo/dao/MemoDao.kt
@@ -0,0 +1,24 @@
+package love.aespa.nemomemo.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import love.aespa.nemomemo.entity.Memo
+
+@Dao
+interface MemoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Memo)
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun update(item: Memo)
+
+    @Query("SELECT * FROM memos ORDER BY id ASC")
+    fun getAllMemosOrderByIdAsc(): Flow<List<Memo>>
+
+    @Query("SELECT * FROM memos WHERE id = :id")
+    fun getMemoById(id: Int): Flow<Memo>
+}
