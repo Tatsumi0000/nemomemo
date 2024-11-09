0a6244f1852a9c644faaf8248b4390971dbbe10d
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-09 22:52:13 +0900

    Add Room settings

---
+++ data/memo/src/commonMain/kotlin/love/aespa/nemomemo/database/MemoDatabase.kt
@@ -0,0 +1,42 @@
+package love.aespa.nemomemo.database
+
+import androidx.room.ConstructedBy
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import love.aespa.nemomemo.entity.Memo
+
+@Database(entities = [Memo::class], version = 1)
+@ConstructedBy(MemoDatabaseConstructor::class)
+abstract class MemoDatabase: RoomDatabase() {
+    abstract fun getDao(): MemoDao
+}
+
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object MemoDatabaseConstructor : RoomDatabaseConstructor<MemoDatabase> {
+    override fun initialize(): MemoDatabase
+}
+
+
+@Dao
+interface MemoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Memo)
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun update(item: Memo)
+
+    @Query("SELECT * FROM memos ORDER BY id ASC")
+    fun getAllMemosOrderByIdAsc(): Flow<List<Memo>>
+
+    @Query("SELECT * FROM memos WHERE id = :id")
+    fun getMemoById(id: Int): Flow<Memo>
+
+}
