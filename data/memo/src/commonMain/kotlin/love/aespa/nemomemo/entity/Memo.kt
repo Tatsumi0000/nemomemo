992e5c1cc6d222960815cbc3f233e3dc5f476943
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-09 22:51:56 +0900

    Add Memos table

---
+++ data/memo/src/commonMain/kotlin/love/aespa/nemomemo/entity/Memo.kt
@@ -0,0 +1,18 @@
+package love.aespa.nemomemo.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+
+@Entity(tableName = "memos")
+data class Memo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val text: String = "",
+    @ColumnInfo(name = "created_at")
+    val createdAt: Date,
+    @ColumnInfo(name = "updated_at")
+    val updatedAd: Date
+)
