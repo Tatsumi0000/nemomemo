dcf3691b81e6ea5ac0f268740db5b4056be74ea9
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-09 22:51:27 +0900

    Add Room library

--- data/memo/build.gradle.kts
+++ data/memo/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.androidxRoom)
 }
 
@@ -10,6 +11,7 @@ kotlin {
     jvm()
 
     sourceSets {
+        val desktopMain by commonMain
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -19,7 +21,13 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // Room
             implementation(libs.room.runtime)
         }
     }
+}
+
+dependencies {
+    add("kspJvm", libs.room.compiler)
 }
