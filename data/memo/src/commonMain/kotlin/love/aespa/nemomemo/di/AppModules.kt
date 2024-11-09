8f37fadc30df35e8d45b312d0f86253ccf041fd2
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-10 03:31:41 +0900

    Rename KoinApp.kt to AppModules.kt

--- data/memo/src/commonMain/kotlin/love/aespa/nemomemo/di/AppModules.kt
+++ data/memo/src/commonMain/kotlin/love/aespa/nemomemo/di/AppModules.kt
@@ -3,7 +3,7 @@ package love.aespa.nemomemo.di
 import love.aespa.nemomemo.database.di.databaseModule
 import love.aespa.nemomemo.repository.di.memoRepositoryModule
 
-fun appModule() = listOf(
+fun appModules() = listOf(
     memoRepositoryModule,
     databaseModule()
 )
