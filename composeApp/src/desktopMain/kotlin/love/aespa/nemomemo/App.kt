12f574eb55c01f9b1996709b7f50f45426e1c471
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-10 14:00:06 +0900

    Rename appModules to dataMemoModule

--- composeApp/src/desktopMain/kotlin/love/aespa/nemomemo/App.kt
+++ composeApp/src/desktopMain/kotlin/love/aespa/nemomemo/App.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.launch
 import love.aespa.nemomemo.database.entity.Memo
-import love.aespa.nemomemo.di.appModules
+import love.aespa.nemomemo.di.dataMemoModule
 import love.aespa.nemomemo.repository.MemoRepository
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -27,7 +27,7 @@ import java.util.Date
 @Preview
 fun App() {
     KoinApplication(application = {
-        modules(appModules())
+        modules(dataMemoModule())
     }) {
     val repository = koinInject<MemoRepository>()
 
--- data/memo/src/commonMain/kotlin/love/aespa/nemomemo/di/DataMemoModule.kt
+++ data/memo/src/commonMain/kotlin/love/aespa/nemomemo/di/DataMemoModule.kt
@@ -3,7 +3,7 @@ package love.aespa.nemomemo.di
 import love.aespa.nemomemo.database.di.databaseModule
 import love.aespa.nemomemo.repository.di.memoRepositoryModule
 
-fun appModules() = listOf(
+fun dataMemoModule() = listOf(
     memoRepositoryModule,
     databaseModule()
 )
