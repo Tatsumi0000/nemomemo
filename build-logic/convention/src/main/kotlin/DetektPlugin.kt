fed8066434d594f3a84c529948cad4cfc8dbb8a6
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-13 22:58:17 +0900

    Create detekt plugins

--- build-logic/convention/src/main/kotlin/DetektPlugin.kt
+++ build-logic/convention/src/main/kotlin/DetektPlugin.kt
@@ -1,18 +1,17 @@
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.getByType
-
+import org.gradle.kotlin.dsl.dependencies
 
 class DetektPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
-            apply("io.gitlab.arturbosch.detekt")
-            val extension = extensions.getByType<DetektExtension>()
+            pluginManager.apply("io.gitlab.arturbosch.detekt")
 
+            dependencies {
+                "detektPlugins"(libs.findLibrary("detektFormatting").get())
+//                "detektPlugins"(libs.findLibrary("twitterComposeRule").get())
+            }
         }
     }
-
 }
