8b69b0eff90ef6a57bc42bb1077e991b8c8522e5
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-13 23:18:10 +0900

    Fix detekt plugin

--- build-logic/convention/src/main/kotlin/DetektPlugin.kt
+++ build-logic/convention/src/main/kotlin/DetektPlugin.kt
@@ -1,12 +1,18 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class DetektPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("io.gitlab.arturbosch.detekt")
+            val extension = extensions.getByType<DetektExtension>()
+            extension.apply {
+                autoCorrect = true
+            }
 
             dependencies {
                 "detektPlugins"(libs.findLibrary("detektFormatting").get())
