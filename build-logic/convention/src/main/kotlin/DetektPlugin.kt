8e42f1d3a4bb81ed2a7d48404689b1b5f2ff411a
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-11-24 02:49:15 +0900

    Add detekt target files

--- build-logic/convention/src/main/kotlin/DetektPlugin.kt
+++ build-logic/convention/src/main/kotlin/DetektPlugin.kt
@@ -9,14 +9,24 @@ class DetektPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("io.gitlab.arturbosch.detekt")
+            val fileTree = project.files("./").asFileTree.matching {
+                include("**/*.kt")
+                include("**/*.kts")
+                exclude("**/build/**")
+                exclude("**/resources/**")
+            }
             val extension = extensions.getByType<DetektExtension>()
             extension.apply {
+                buildUponDefaultConfig = true
                 autoCorrect = true
+                config.setFrom(project.files("${project.rootProject.rootDir}/config/detekt.yml"))
+                debug = true
+                source.setFrom(fileTree)
             }
 
             dependencies {
                 "detektPlugins"(libs.findLibrary("detektFormatting").get())
-//                "detektPlugins"(libs.findLibrary("twitterComposeRule").get())
+                "detektPlugins"(libs.findLibrary("detektComposeRule").get())
             }
         }
     }
